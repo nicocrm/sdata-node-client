d7c60f66648725da3efd27fe4033f3810589706c
Nicolas Galler <ngaller@gmail.com>
2017-08-05 04:49:31 -0400

    Extend the API to return promise, when callback not provided

--- index.js
+++ index.js
@@ -2,162 +2,163 @@
 
 'use strict';
 
-var requestBase = require('request'),
-    util = require('util');
+var requestBase = require('request-promise-native'),
+  util = require('util');
 
 function SDataService(sdataUri, username, password) {
-    var _request = requestBase;
-    var service = {
-        setAuthenticationParameters: function (newUsername, newPassword) {
-            _request = _request.defaults({
-                auth: {
-                    'user': newUsername,
-                    'pass': newPassword
-                },
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                json: true
-            });
+  var _request = requestBase;
+  var service = {
+    setAuthenticationParameters: function (newUsername, newPassword) {
+      _request = _request.defaults({
+        auth: {
+          'user': newUsername,
+          'pass': newPassword
         },
-
-        read: function (resourceKind, where, queryArgs, callback) {
-            // summary:
-            //  Retrieve SData resources matching the specified criteria
-            // parameters:
-            //  resourceKind
-            //  where
-            //  queryArgs (optional) object with properties to be added to the request (e.g. {select: 'AccountName'})
-            //  callback: function(error, data):
-            //     - if the call is successful, will be called with data
-            //     - if there is an error, will be called with an error object with the properties:
-            //                 message: description of the error (first error message returned from sdata if available)
-            //                 errors: populated with errors returned from sdata, if any
-            //                 statusCode: http status code, if available
-
-            var url = sdataUri + resourceKind + '?format=json';
-            if (where) {
-                // this must be encoded explicitly because Angular will encode a space to a + (conforming to RFC)
-                // which sdata cannot parse
-                url += '&where=' + encodeURIComponent(where);
-            }
-            if (queryArgs) {
-                if (typeof (queryArgs) == 'function' && !callback) {
-                    callback = queryArgs;
-                } else {
-                    for (var k in queryArgs) {
-                        if (queryArgs.hasOwnProperty(k))
-                            url += '&' + k + '=' + encodeURIComponent(queryArgs[k]);
-                    }
-                }
-            }
-
-            _request.get(url, handleSdataResponse(200, callback));
+        headers: {
+          'Content-Type': 'application/json'
         },
+        json: true,
+        resolveWithFullResponse: true
+      });
+    },
 
-        create: function (resourceKind, data, callback) {
-            // summary:
-            //  Create resource
-            // parameters:
-            //  resourceKind: string (e.g. accounts)
-            //  data: object (content of the record to create)
-            //  callback: function(data,error): handler for returned data (see callback documentation under read)
-            var url = sdataUri + resourceKind + '?format=json';
-            _request({
-                method: 'POST',
-                uri: url,
-                body: data
-            }, handleSdataResponse(201, callback));
-        },
+    read: function (resourceKind, where, queryArgs, callback) {
+      // summary:
+      //  Retrieve SData resources matching the specified criteria
+      // parameters:
+      //  resourceKind
+      //  where
+      //  queryArgs (optional) object with properties to be added to the request (e.g. {select: 'AccountName'})
+      //  callback: function(error, data):
+      //     - if the call is successful, will be called with data
+      //     - if there is an error, will be called with an error object with the properties:
+      //                 message: description of the error (first error message returned from sdata if available)
+      //                 errors: populated with errors returned from sdata, if any
+      //                 statusCode: http status code, if available
 
-        update: function (resourceKind, data, callback) {
-            // summary:
-            //  Update designated resource.  The id ($key) must be provided as part of the data.
-            var url = sdataUri + resourceKind + '("' + data.$key + '")?format=json';
-            _request({
-                method: 'PUT',
-                uri: url,
-                body: data
-            }, handleSdataResponse(200, callback));            
-        },
-        
-        upsert: function(resourceKind, data, callback) {
-            // summary:
-            //  Convenience method combining insert + update.
-            //  If the data has a $key property, update will be called, otherwise insert.
-            (data.$key ? service.update : service.create)(resourceKind, data, callback);            
-        },
-
-        delete: function (resourceKind, key, callback) {
-            // summary:
-            //  delete designated resource.
-            // Note that when invoked successfully the callback will not be passed any data.
-            var url = sdataUri + resourceKind + '("' + key + '")?format=json';
-            _request({
-                method: 'DELETE',
-                uri: url                
-            }, handleSdataResponse(200, callback));
-        },
-
-        callBusinessRule: function (resourceKind, operationName, recordId, parameters, callback) {
-            var payload = {
-                $name: operationName,
-                request: {
-                    entity: {
-                        $key: recordId
-                    }
-                }
-            };
-            if (parameters){
-                util._extend(payload.request, parameters);
-            }
-            var url = sdataUri + resourceKind + '/$service/' + operationName + '?format=json';
-            _request({
-                method: 'POST',
-                uri: url,
-                body: payload
-            }, function(err, r, body) {
-                if(!err && body && body.response)
-                    // get the inside response, for business rule calls
-                    callback(null, body.response);
-                else
-                    handleSdataResponse(200, callback)(err, r, body);
-            });
+      var url = sdataUri + resourceKind + '?format=json';
+      if (where) {
+        // this must be encoded explicitly because Angular will encode a space to a + (conforming to RFC)
+        // which sdata cannot parse
+        url += '&where=' + encodeURIComponent(where);
+      }
+      if (queryArgs) {
+        if (typeof (queryArgs) == 'function' && !callback) {
+          callback = queryArgs;
+        } else {
+          for (var k in queryArgs) {
+            if (queryArgs.hasOwnProperty(k))
+              url += '&' + k + '=' + encodeURIComponent(queryArgs[k]);
+          }
         }
-    };
+      }
 
-    if (username)
-        service.setAuthenticationParameters(username, password);
+      return handleSdataResponse(_request.get(url), 200, callback)
+    },
 
-    return service;
+    create: function (resourceKind, data, callback) {
+      // summary:
+      //  Create resource
+      // parameters:
+      //  resourceKind: string (e.g. accounts)
+      //  data: object (content of the record to create)
+      //  callback: function(data,error): handler for returned data (see callback documentation under read)
+      var url = sdataUri + resourceKind + '?format=json';
+      return handleSdataResponse(_request({
+        method: 'POST',
+        uri: url,
+        body: data
+      }), 201, callback);
+    },
 
-    ///////////////
+    update: function (resourceKind, data, callback) {
+      // summary:
+      //  Update designated resource.  The id ($key) must be provided as part of the data.
+      var url = sdataUri + resourceKind + '("' + data.$key + '")?format=json';
+      return handleSdataResponse(_request({
+        method: 'PUT',
+        uri: url,
+        body: data
+      }), 200, callback);
+    },
 
-    function handleSdataResponse(expectedStatusCode, callback) {
-        return function (e, r, body) {
-            if(!callback)
-                return;
-            
-            if (!e && r.statusCode == expectedStatusCode) {                
-                callback(null, body);
-            } else {
-                var error;
-                if(!r)
-                    error = { message: 'Unable to connect: ' + e }
-                else if(r.statusCode == 401)
-                    error = { message: 'Authentication failed' }
-                else if(body && util.isArray(body)) 
-                    error = { message: body[0].message, errors: body };
-                else 
-                    error = { message: 'Unknown sdata error' };
-                if(r) {
-                    error.statusCode = r.statusCode;
-                }
-                
-                callback(error, null);
-            }
+    upsert: function(resourceKind, data, callback) {
+      // summary:
+      //  Convenience method combining insert + update.
+      //  If the data has a $key property, update will be called, otherwise insert.
+      (data.$key ? service.update : service.create)(resourceKind, data, callback);
+    },
+
+    delete: function (resourceKind, key, callback) {
+      // summary:
+      //  delete designated resource.
+      // Note that when invoked successfully the callback will not be passed any data.
+      var url = sdataUri + resourceKind + '("' + key + '")?format=json';
+      return handleSdataResponse(_request({
+        method: 'DELETE',
+        uri: url
+      }), 200, callback);
+    },
+
+    callBusinessRule: function (resourceKind, operationName, recordId, parameters, callback) {
+      var payload = {
+        $name: operationName,
+        request: {
+          entity: {
+            $key: recordId
+          }
         }
+      };
+      if (parameters){
+        util._extend(payload.request, parameters);
+      }
+      var url = sdataUri + resourceKind + '/$service/' + operationName + '?format=json';
+      var p = handleSdataResponse(_request({
+        method: 'POST',
+        uri: url,
+        body: payload
+      }), 200)
+        .then(function(body) {
+          // get the inside response, for business rule calls
+          return body.response ? body.response : body
+        })
+      if(callback) {
+        p = p.then(function(r) { callback(null, r) }, callback)
+      }
+      return p;
     }
+  };
+
+  if (username)
+    service.setAuthenticationParameters(username, password);
+
+  return service;
+
+  ///////////////
+
+  function handleSdataResponse(requestPromise, expectedStatusCode, callback) {
+    var p = requestPromise.then(function(response) {
+      var body = response.body
+      if(response.statusCode !== expectedStatusCode) {
+        var error = { message: 'Unknown sdata error' }
+        if(response.statusCode == 401)
+          error = { message: 'Authentication failed' }
+        else if(body && util.isArray(body))
+          error = { message: body[0].message, errors: body };
+        return Promise.reject(error)
+      } else {
+        return body
+      }
+    })
+    if(callback) {
+      p = p.then(function(body) {
+        callback(null, body)
+      }, function(error) {
+        callback(error)
+      })
+    }
+    return p
+  }
 }
 
 module.exports = SDataService;
--- tests/sdataSpec.js
+++ tests/sdataSpec.js
@@ -1,102 +1,115 @@
 'use strict';
 
-var SDATA_TEST_URL = 'http://vmng-slx81.sssworld-local.com:3012/sdata/slx/dynamic/-/';
+// these tests use the live sdata server and thus will not work outside of the VPN,
+// they can also fail due to timeouts
+var SDATA_TEST_URL = 'http://vmng-slx83.sssworld-local.com:3001/sdata/slx/dynamic/-/';
 var expect = require('chai').expect;
 var sdataProvider = require('../index');
 var sdata = sdataProvider(SDATA_TEST_URL, 'admin', '');
 
 describe('sdata service - tests using actual SData server', function() {
-    this.timeout(5000);
-    
-    it('should retrieve accounts', function(done) {
-        sdata.read('accounts', 'AccountName like \'A%\'', { select: 'AccountName,Address/City' }, function(error, data) {
-            expect(error).to.not.be.ok;
-            expect(data).to.be.ok;
-            expect(data).to.have.property('$totalResults');
-            expect(data.$resources[0]).to.have.property('AccountName');
-            expect(data.$resources[0]).to.not.have.property('Type');
-            expect(data.$resources[0].Address).to.have.property('City');
-            done();
-        });
+  this.timeout(7500);
+
+  it('should retrieve accounts', function(done) {
+    sdata.read('accounts', 'AccountName like \'A%\'', { select: 'AccountName,Address/City' }, function(error, data) {
+      expect(error).to.not.be.ok;
+      expect(data).to.be.ok;
+      expect(data).to.have.property('$totalResults');
+      expect(data.$resources[0]).to.have.property('AccountName');
+      expect(data.$resources[0]).to.not.have.property('Type');
+      expect(data.$resources[0].Address).to.have.property('City');
+      done();
     });
-    it('should return error when querying an unavailable site', function(done) {
-        var sdata2 = sdataProvider('http://invaliddomainnameXXXX.com');
-        sdata2.read('accounts', 'AccountName like \'\'', function(error, data) {
-            expect(error).to.be.ok;
-            done();
-        });
+  });
+  it('should return error when querying an unavailable site', function(done) {
+    var sdata2 = sdataProvider('http://invaliddomainnameXXXX.com');
+    sdata2.read('accounts', 'AccountName like \'\'', function(error, data) {
+      expect(error).to.be.ok;
+      done();
     });
-    it('should return error when authentication is not valid', function(done) {
-        var sdata = sdataProvider(SDATA_TEST_URL);
-        sdata.read('accounts', 'AccountName like \'\'', function(error, data) {
-            expect(error).to.be.ok;
-            expect(error.statusCode).to.equal(401);
-            done();
-        });
+  });
+  it('should return error when authentication is not valid', function(done) {
+    var sdata = sdataProvider(SDATA_TEST_URL);
+    sdata.read('accounts', 'AccountName like \'\'', function(error, data) {
+      expect(error).to.be.ok;
+      expect(error.statusCode).to.equal(401);
+      done();
     });
-    it('should create accounts', function(done) {
-        var acc = {
-            AccountName: 'Foo'
-        };
-        sdata.create('accounts', acc, function(error, data) {
-            expect(error).to.not.be.ok;
-            expect(data).to.be.ok;
-            expect(data).to.have.property('$key');
-            sdata.delete('accounts', data.$key);
-            done();
-        });
+  });
+  it('should create accounts', function(done) {
+    var acc = {
+      AccountName: 'Foo'
+    };
+    sdata.create('accounts', acc, function(error, data) {
+      expect(error).to.not.be.ok;
+      expect(data).to.be.ok;
+      expect(data).to.have.property('$key');
+      sdata.delete('accounts', data.$key);
+      done();
     });
-    it('should update accounts', function(done) {
-        var acc = {
-            AccountName: 'Foo'
-        };
-        sdata.create('accounts', acc, function(error, data) {
-            sdata.update('accounts', {
-                '$key': data.$key,
-                AccountName: 'Foo++'
-            }, function(error, data) {
-                expect(error).to.not.be.ok;
-                expect(data.AccountName).to.equal('Foo++');                
-                sdata.delete('accounts', data.$key);  // cleanup
-                done();
-            });
-        });
-    });    
-    it('should create accounts using upsert', function(done) {
-        var acc = {
-            AccountName: 'Foo'
-        };
-        sdata.upsert('accounts', acc, function(error, data) {
-            expect(error).to.not.be.ok;
-            expect(data.$key).to.be.ok;
-            sdata.delete('accounts', data.$key); // cleanup
-            done();
-        });
+  });
+  it('should create accounts, using promise', function(done) {
+    var acc = {
+      AccountName: 'Foo'
+    };
+    sdata.create('accounts', acc).then(function(data) {
+      expect(data).to.be.ok;
+      expect(data).to.have.property('$key');
+      sdata.delete('accounts', data.$key);
+      done();
     });
-    it('should delete accounts', function(done) {
-        var acc = {
-            AccountName: 'Foo'
-        };
-        sdata.create('accounts', acc, function(error, data) {
-            sdata.delete('accounts', data.$key, function(error, data) {
-                expect(error).to.not.be.ok;
-                expect(data).to.be.undefined;
-                done();
-            });
-        });        
+  })
+  it('should update accounts', function(done) {
+    var acc = {
+      AccountName: 'Foo'
+    };
+    sdata.create('accounts', acc, function(error, data) {
+      sdata.update('accounts', {
+        '$key': data.$key,
+        AccountName: 'Foo++'
+      }, function(error, data) {
+        expect(error).to.not.be.ok;
+        expect(data.AccountName).to.equal('Foo++');
+        sdata.delete('accounts', data.$key);  // cleanup
+        done();
+      });
     });
-    it('should call a business rule', function(done) {
-        var acc = {
-            AccountName: 'Foo'
-        };
-        sdata.create('accounts', acc, function(error, data) {
-            expect(error).to.not.be.ok;            
-            sdata.callBusinessRule('accounts', 'CanChangeOwner', data.$key, null, function(error, data) {
-                expect(error).to.not.be.ok;
-                expect(data.Result).to.be.true;
-                sdata.delete('accounts', data.$key); // cleanup
-                done();
-            });            
-        });
+  });
+  it('should create accounts using upsert', function(done) {
+    var acc = {
+      AccountName: 'Foo'
+    };
+    sdata.upsert('accounts', acc, function(error, data) {
+      expect(error).to.not.be.ok;
+      expect(data.$key).to.be.ok;
+      sdata.delete('accounts', data.$key); // cleanup
+      done();
     });
+  });
+  it('should delete accounts', function(done) {
+    var acc = {
+      AccountName: 'Foo'
+    };
+    sdata.create('accounts', acc, function(error, data) {
+      sdata.delete('accounts', data.$key, function(error, data) {
+        expect(error).to.not.be.ok;
+        expect(data).to.be.undefined;
+        done();
+      });
+    });
+  });
+  it('should call a business rule', function(done) {
+    var acc = {
+      AccountName: 'Foo'
+    };
+    sdata.create('accounts', acc, function(error, data) {
+      expect(error).to.not.be.ok;
+      sdata.callBusinessRule('accounts', 'CanChangeOwner', data.$key, null, function(error, data) {
+        expect(error).to.not.be.ok;
+        expect(data.Result).to.be.true;
+        sdata.delete('accounts', data.$key); // cleanup
+        done();
+      });
+    });
+  });
 });
